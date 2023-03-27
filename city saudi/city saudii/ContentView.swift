f52f3554e0e0e8cf266d64749a02ab60dc588f7d
sqoon <93549806+DHME1@users.noreply.github.com>
2023-03-27 09:04:00 +0300

    Add files via upload

---
+++ city saudi/city saudii/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Parallax-SwiftUI
+//
+//  Created by Kevin ahmad on 16/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
